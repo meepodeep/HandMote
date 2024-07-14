c9f983f14202d844fd8bc98507447450fdec2dfe
meepodeep <nbltmepodep@gmail.com>
2024-07-14 01:34:32 -0400

    Added mouse functionality into multi threading

--- HandMote.py
+++ HandMote.py
@@ -7,22 +7,20 @@ import threading
 
 cxInverse = 0
 pag.FAILSAFE = False
-cx = 0
-cy = 0
+cx_global = 0
+cy_global = 0
 ret = False
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
-def MoveMouse(cx, cy):
-    while True:
-        cxInverse = (cx *-1) + 1920
-        pag.moveTo(2.2*cxInverse, 2*cy,.01)
 
    
 def FindHands(cam,):
-
+    global cx_global
+    global cy_global
     while True:
+        
         ret, frame = cam.read()
         imgRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.resize(frame, (1920, 1080))
@@ -34,11 +32,16 @@ def FindHands(cam,):
                 for id, lm in enumerate(handLms.landmark):
                     h, w, c = frame.shape
                     cx, cy = int(lm.x * w), int(lm.y * h)
-                    if id == 5:
+                    cy_global = cy
+                    cx_global = cx
+                    if id == 0:
                         cv2.circle(frame, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+                    
+                    if id == 2:
+                        cv2.circle(frame, (cx, cy), 20, (255, 0, 255), cv2.FILLED)
                         
                 mpDraw.draw_landmarks(frame, handLms, mpHands.HAND_CONNECTIONS)
-
+    
             
 
         cv2.imshow('frame', frame)
@@ -46,12 +49,24 @@ def FindHands(cam,):
             break
     cap.release()
     cv2.destroyAllWindows()
-
+    
+    
+def MoveMouse(cxInverse,):
+    global cx_global
+    global cy_global
+    while True:
+        print(cx_global, cy_global)
+        cxInverse = (cx_global *-1) + 1920
+        pag.moveTo(2*cxInverse, 2*cy_global,.01)
+        
+        
 if __name__ == '__main__':
     t1 = threading.Thread(target = FindHands,args=(cap,))
-    t2 = threading.Thread(target = MoveMouse,args=(cx,cy))
+    t2 = threading.Thread(target = MoveMouse,args=(cxInverse,))
     t1.start()
     t2.start()
+    t1.join()
+    t2.join()
     
 
      
